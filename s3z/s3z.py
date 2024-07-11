396b533c54515d2b89b6786430f22840e893e2be
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-07-10 20:07:05 -0400

    polish the boto demo

--- s3z/s3z.py
+++ s3z/s3z.py
@@ -1,48 +1,67 @@
 #!/usr/bin/env python
 
 import argparse
+import os
+
 import boto3
 import openziti
-import os
 
 
 def configure_openziti(ziti_identity_file, bucket_endpoint):
-    print(f"Configuring openziti with identity file: {ziti_identity_file}."
-          f"Ensure Dial Service Policy grants intercept '{bucket_endpoint}'.")
+    print(f"Configuring openziti with identity file: {ziti_identity_file}.",
+          f"Ensure service configs match regional bucket endpoint'.")
     return openziti.load(ziti_identity_file)
 
 
 def push_logs_to_s3(bucket_name, bucket_endpoint, push_log_dir, object_prefix):
-    # Create a boto3 S3 client
-    s3_client = boto3.client('s3', endpoint_url=bucket_endpoint)
+    if bucket_endpoint:
+        s3 = boto3.client('s3', endpoint_url=bucket_endpoint)
+    else:
+        s3 = boto3.client('s3')
 
-    # Iterate over the files in the directory
     for file_name in os.listdir(push_log_dir):
         if file_name.endswith(".log"):
             file_path = os.path.join(push_log_dir, file_name)
-            # Upload the file to the S3 bucket
             with openziti.monkeypatch():
                 if object_prefix:
-                    s3_client.upload_file(file_path, bucket_name, f"{object_prefix}/{file_name}")
-                    print(f"Uploaded {file_path} to {bucket_name}/{object_prefix}.")
+                    s3.upload_file(file_path, bucket_name,
+                                   f"{object_prefix}/{file_name}")
+                    print(f"Uploaded {file_path} to"
+                          f"{bucket_name}/{object_prefix}.")
                 else:
-                    s3_client.upload_file(file_path, bucket_name, file_name)
+                    s3.upload_file(file_path, bucket_name, file_name)
                     print(f"Uploaded {file_path} to {bucket_name}.")
 
 
-
 if __name__ == "__main__":
-    # Parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ziti-identity-file', required=True, help='Ziti identity file')
-    parser.add_argument('--bucket-name', required=True, help='bucket name')
-    parser.add_argument('--bucket-endpoint', required=True, help='private bucket endpoint')
-    parser.add_argument('--object-prefix', required=False, default='', help='object key prefix in bucket for pushed files')
-    parser.add_argument('--push-log-dir', required=False, default='.', help='directory with *.log files to push to bucket')
+    parser.add_argument('--ziti-identity-file', required=True,
+                        help='Ziti identity file')
+    parser.add_argument('--bucket-name', required=True,
+                        help='bucket name')
+    parser.add_argument('--bucket-endpoint', required=False, default='',
+                        help='private bucket endpoint')
+    parser.add_argument('--object-prefix', required=False, default='',
+                        help='object key prefix in bucket for pushed files')
+    parser.add_argument('--push-log-dir', required=False, default='.',
+                        help='directory with *.log files to push to bucket')
     args = parser.parse_args()
 
-    # Configure openziti
-    configure_openziti(args.ziti_identity_file, args.bucket_endpoint)
+    sts = boto3.client('sts')
+    caller = sts.get_caller_identity()
+    print(f"\nAuthenticated to AWS as:",
+          f"UserId: {caller.get('UserId')}",
+          f"Account: {caller.get('Account')}",
+          f"Arn: {caller.get('Arn')}\n", sep="\n\t")
 
-    # Push logs to bucket
-    push_logs_to_s3(args.bucket_name, args.bucket_endpoint, args.push_log_dir, args.object_prefix)
+    configure_openziti(
+        args.ziti_identity_file,
+        args.bucket_endpoint
+    )
+
+    push_logs_to_s3(
+        args.bucket_name,
+        args.bucket_endpoint,
+        args.push_log_dir,
+        args.object_prefix
+    )
